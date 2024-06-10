b29a18efba3c7135f4fbf17d4e8645fd0020533d
Momtchil Momtchev <momtchil@momtchev.com>
2024-06-10 13:46:54 +0200

    replace the package name for the unit tests

--- test/util-test.js
+++ test/util-test.js
@@ -102,7 +102,7 @@ test('urlTemplate() returns different templates based on pkg and rc', function (
   t.equal(t8, 'http://overriden-url.com/overriden-path/{tag_prefix}{version}/{name}-v{version}-{runtime}-v{abi}-{platform}{libc}-{arch}.tar.gz', '--download with binary defined and host mirror override')
   const o9 = { pkg: require('../package.json'), download: true }
   const t9 = util.urlTemplate(o9)
-  t.equal(t9, 'https://github.com/prebuild/prebuild-install/releases/download/{tag_prefix}{version}/{name}-v{version}-{runtime}-v{abi}-{platform}{libc}-{arch}.tar.gz', '--download with no arguments, no pkg.binary, no host mirror, default format')
+  t.equal(t9, 'https://github.com/mmomtchev/prebuild-install/releases/download/{tag_prefix}{version}/{name}-v{version}-{runtime}-v{abi}-{platform}{libc}-{arch}.tar.gz', '--download with no arguments, no pkg.binary, no host mirror, default format')
   t.end()
 })
 
